0c3e020aa44202db1b3d7a1291cb9752ca5118b3
pedroseabra1091 <pedroseabra1091@gmail.com>
2018-06-27 11:02:41 +0100

    Removed data layer files

--- app/src/main/java/com/example/pedrobatista/kotlinstarter/data/Database.kt
+++
@@ -1,8 +0,0 @@
-package com.example.pedrobatista.androidnavigation.data
-
-import android.arch.persistence.room.RoomDatabase
-import com.example.pedrobatista.androidnavigation.data.dao.MoveDao
-
-abstract class Database : RoomDatabase() {
-    abstract fun moveDao() : MoveDao
-}
